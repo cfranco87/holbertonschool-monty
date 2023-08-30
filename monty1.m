594b8e074bb1df8029cbd6ccdd6ced601f52d886
cfranco87 <carlosfrancoart@gmail.com>
2023-08-29 20:26:45 -0700

    starting monty

---
+++ monty1.m
@@ -0,0 +1,25 @@
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void push(int value);
+
+int main(void)
+{
+	int n;
+	
+	if (n != 0)
+	{
+		printf("push integer");
+		exit(STDERR, "EXIT_FAILURE\n");
+	}
+
+	strcpy(&str);
+
+	val = atoi(str);
+	printf(str);
+
+	return (0); 
+}
